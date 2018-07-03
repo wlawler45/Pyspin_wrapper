2c0014667c9c627c210aec842c930d9e534f4ab0
William <williamgmlawler@yahoo.com>
2018-07-03 13:51:26 -0400

    Tested wrapper and verified components function

--- scripts/pyspin_library_test.py
+++ scripts/pyspin_library_test.py
@@ -1,22 +1,31 @@
-from pyspin_library import Pyspin_VideoCapture
+
+
+import pyspin_wrapper.pyspin_library as wrapper
+
 import numpy as np
 import cv2
 
 def image_save_and_load(image):
 	image.Save("Testimage.jpg")
-	return cv2.imread("Testimage.jpg",1)
-	
+	loaded=cv2.imread("Testimage.jpg",1)
+	return cv2.resize(loaded,(1920,1080))
 
-if __name__== 'main':
+if __name__== '__main__':
 	camname="topcam"
 	serialnum="18080264"
-	camera_obj=Pyspin_VideoCapture(camname,serialnum)
+	print "hello"
+	camera_obj=wrapper.Pyspin_VideoCapture(camname,serialnum,{})
 	camera_obj.printall_camera_ids()
-	camera_obj.print_camera_values()
+	
+	
+
 	camera_obj.open_camera()
+	camera_obj.print_camera_values()
+
+
 	image_out=camera_obj.read_frame()
 	
-	#image_out=image_save_and_load(image_out)
+	image_out=image_save_and_load(image_out)
 	cv2.imshow('image',image_out)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
